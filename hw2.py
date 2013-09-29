28cac9e71e07d6c01fc0ae0974e82f8fb7ab6d11
Jill <jillian.munson@stonybrook.edu>
2013-09-29 15:28:03 -0400

    better heuristic

--- hw2.py
+++ hw2.py
@@ -1,6 +1,6 @@
-import fileinput
+import fileinput, random, time
 
-def concat(args):
+def concat(*args):
     result = ''
     for arg in args:
         result = _concat(result, arg)
@@ -23,42 +23,82 @@ def _concat(a, b):
     return a + b[n:]
 
 def _overlap(m, n):
+    result = {}
+    prefix = ""
+    suffix = ""
     overlap = 0
     for i in xrange(len(m)):
         for j in xrange(len(n)):
             current_overlap = 0
             if(m[i] == n[j]):
                 current_overlap = current_overlap + 1
-                while((i + current_overlap) < len(m) - 1) and ((j + current_overlap) < len(n) - 1):
+                while((i + current_overlap) < len(m)-1) and ((j + current_overlap) < len(n)-1):
                     if(m[i + current_overlap] == n[j + current_overlap]):
                         current_overlap = current_overlap + 1
                     else:
                         break
                 if(current_overlap > overlap):
                     overlap = current_overlap
-    return overlap
+                    if((i + current_overlap) >= len(m)):
+                        prefix = m
+                        suffix = n
+                    else:
+                        prefix = n
+                        suffix = m
+    result['overlap'] = overlap
+    result['prefix'] = prefix
+    result['suffix'] = suffix
+    return result
+
+
+def gen_strings(input):
+    strings = []
+    random.seed()
+    size = random.randint(2, len(input) - 2)
+    sample = random.randint(1, len(input) + 1000)
+    print("Fragment size: " + str(size) + ", " + str(sample) + " fragments")
+    while sample > 0:
+        temp_index = random.randint(0, len(input))
+        if(temp_index + size) < len(input):
+            strings.append(input[temp_index:temp_index + size])
+            sample = sample - 1
+    return strings
+
+
+
+def duplicates(frags):
+    unique = {}
+    for frag in frags:
+        keys = unique.keys()
+        if(frag in keys):
+            unique[str(frag)] = unique[str(frag)] + 1
+        else:
+            unique[str(frag)] = 1
+    return unique
 
 if __name__ == '__main__':
-    strings = []
     for line in fileinput.input():
-        strings.append(str(line)[0:len(line) - 1])
-    superstring = strings[0]
-    while(len(strings) != 0):
-        overlap = 0
-        candidate = ""
-        for i in xrange(1, len(strings)):
-            temp = _overlap(superstring, strings[i])
-            if (temp > overlap):
-                candidate = strings[i]
-                overlap = temp
-        print("current: " + superstring + ", candidate: " + candidate + ".  Overlap = " + str(overlap))
-        if(len(candidate) > 0):
-            if(ord(candidate[0]) < ord(superstring[0])):
-                superstring = concat(candidate, superstring)
-            else:
-                superstring = concat(superstring, candidate)
-            print("super so far: " + superstring)
-            strings.remove(candidate)
-        else:
-            strings.remove(strings[0])
-    print(superstring)
+        start_time = time.time()
+        random.seed()
+        strings = gen_strings(line)
+        result = duplicates(strings).keys()
+        print("Unique fragments(" + str(len(result)) + "): " + str(result))
+        while(len(result) > 1):
+            stringm = ""
+            stringn = ""
+            overlap = 0
+            for r in result:
+                for s in result:
+                    if(r != s):
+                        temp = _overlap(r, s)
+                        if(temp['overlap'] > overlap):
+                            overlap = temp['overlap']
+                            stringm = temp['prefix']
+                            stringn = temp['suffix']
+            result.append(concat(stringm, stringn))
+            if(len(stringm) >0):
+                result.remove(stringm)
+            if(len(stringn) >0):
+                result.remove(stringn)
+        print("Superstring:" + str(result) + ", Length: " + str(len(result[0])))
+        print("Time to execute: " + str(time.time() - start_time) + ", Length of input: " + str(len(line)))
